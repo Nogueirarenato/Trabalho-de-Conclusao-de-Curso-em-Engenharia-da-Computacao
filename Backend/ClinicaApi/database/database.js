ef70025f18ec84e64b20ae5e64ff0df640d772a6
Renato Nogueira <renatonogueirasenai@gmail.com>
2023-10-23 19:50:51 -0300

    up

--- Backend/ClinicaApi/database/database.js
+++ Backend/ClinicaApi/database/database.js
@@ -1,7 +1,23 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('apirenatonogue', 'apirenatonogue', 'Banana33', {
-    host: 'mysql.apirenatonogueira.kinghost.net',
+//Banco de Dados KingHost
+
+// const connection = new Sequelize('apirenatonogue', 'apirenatonogue', 'Banana33', {
+//     host: 'mysql.apirenatonogueira.kinghost.net',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//         //useUTC: false, //for reading from database
+//         dateStrings: true,
+//         typeCast: true
+//     },
+//     timezone: '-03:00'
+    
+// });
+
+
+//Banco de Dados local
+const connection = new Sequelize('clinicaapi', 'root', 'banana', {
+    host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
         //useUTC: false, //for reading from database
